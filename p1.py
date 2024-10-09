171a2a3d5797e3b71fd3d2fb778159bc27dff070
Negar Azad <na1381ab1381@gmail.com>
2024-10-09 12:45:07 +0330

    Update p1.py

--- p1.py
+++ p1.py
@@ -15,11 +15,18 @@ most_probable_k = k[np.argmax(probabilities)]
 print(f"بالاترین احتمال برای k: {most_probable_k}")
 
 # رسم نمودار
+#تنظیم کردن اندازه نمودار
 plt.figure(figsize=(10, 6))
+#رسم کردن نمودار توزیع دو جمله ای
 plt.plot(k, probabilities, label='Binomial distribution')
+#نامگذاری محور ها
 plt.xlabel('Number of people online')
 plt.ylabel('possibility')
+#عنوان دهی به خود نمودار
 plt.title('Binomial distribution for the number of people online')
+#راهنمای نمودار
 plt.legend()
+#شبکه بندی کردن نمودار
 plt.grid(True)
+#نمایش دادن نمودار
 plt.show()
